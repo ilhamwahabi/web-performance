ed7e495424b1af0d1e5cd9c3965cbd5b0bc61ec4
iwgx <wahabiputra@gmail.com>
2020-06-18 14:40:41 +0700

    Fix image

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-function Image(props) {
-  return <img {...props} />;
+function Image({ src }) {
+  console.log(src);
+
+  return (
+    <div
+      style={{
+        maxWidth: 1200,
+        width: "80%",
+        height: "100%",
+        backgroundImage: `url(${src})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    />
+  );
 }
 
 export default Image;
